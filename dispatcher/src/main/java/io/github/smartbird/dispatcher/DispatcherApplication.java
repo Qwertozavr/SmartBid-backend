9e0b1af89d7257f21f4aa0172009e8a3154f2ce0
Biliktuev Bator <batojjk@gmail.com>
2026-05-31 22:12:33 +0300

    finished dispatcher version 1.0

--- dispatcher/src/main/java/io/github/smartbird/dispatcher/DispatcherApplication.java
+++ dispatcher/src/main/java/io/github/smartbird/dispatcher/DispatcherApplication.java
@@ -1,5 +1,6 @@
 package io.github.smartbird.dispatcher;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
